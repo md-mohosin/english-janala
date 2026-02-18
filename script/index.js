7cf8188570eefb5e93c08c9fcca0c14c978def05
md mohosin <mdmohosin.web@gmail.com>
2026-02-18 09:02:20 +0600

    loadd word by level

--- script/index.js
+++ script/index.js
@@ -12,9 +12,10 @@ const displyLesson = (lessons) => {
     lessonContainer.innerHTML = ''
     lessons.forEach(lesson => {
         const div = document.createElement("div")
-        div.innerHTML = `<button class="btn btn-outline btn-primary"><i
+        div.innerHTML = `<button onclick='loadWord(${lesson.level_no})' class="btn btn-outline btn-primary"><i
                             class="fa-solid fa-book-open"></i>${lesson.lessonName}</button>`
         lessonContainer.appendChild(div)
+
     })
 }
 
@@ -22,8 +23,8 @@ loadLessons()
 
 
 
-const loadWrod = () => {
-    fetch("https://openapi.programming-hero.com/api/level/5")
+const loadWord = (level) => {
+    fetch(`https://openapi.programming-hero.com/api/level/${level}`)
         .then(res => res.json())
         .then(data => console.log(data))
 }
